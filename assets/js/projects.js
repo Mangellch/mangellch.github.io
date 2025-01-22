8bd9da497455d33ca2831b544c0003edac55466a
Mariangel <mariangelaraviches@gmail.com>
2025-01-21 22:01:59 -0300

    update project and adding another one

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -5,13 +5,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const translations = {
         es: {
             welcome: "Bienvenid@ a mi mundo digital",
-            description: "Desarrolladora web en constante aprendizaje",
+            description: "Transformando ideas en soluciones digitales",
             projects: "Mis Proyectos",
             projectsDesc: "He desarrollado proyectos web utilizando HTML, CSS, JavaScript y Bootstrap para crear interfaces responsivas, funcionales y atractivas. HTML proporciona una estructura clara, CSS mejora el diseño visual y la adaptabilidad, JavaScript agrega interactividad, y Bootstrap asegura un desarrollo ágil y responsive. Cada proyecto refleja mi enfoque en la eficiencia y la experiencia del usuario.",
             projectTitles: [
                 "Front End Store",
                 "Landing Page",
-                "Iguana Page",
+                "Festival De Musica",
                 "Cuppon Page",
                 "Inmobiliaria",
                 "Todo List",
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
             projectDescriptions: [
                 "Un proyecto de e-commerce que simula una tienda en línea con un diseño responsive, utilizando HTML y CSS.",
                 "Un blog sencillo desarrollado con HTML y CSS que permite la visualización de artículos relacionados con el mundo del café.",
-                "Un sitio web con temática de iguanas, utilizando HTML y CSS. La página presenta un diseño atractivo con información sobre estas criaturas y una interfaz fácil de navegar.",
+                "Este proyecto es una página web interactiva diseñada para promocionar y gestionar un festival de música. El sitio ofrece una experiencia visual y dinámica para los usuarios interesados en el evento, con una interfaz intuitiva y moderna.",
                 "Un proyecto web que simula una página de cupones y descuentos. Utiliza HTML, Bootstrap y CSS para crear una interfaz visual atractiva y funcional que permite a los usuarios visualizar diversas promociones.",
                 "Una aplicación web de inmobiliaria que permite mostrar propiedades en venta y alquiler. Desarrollado con HTML, CSS, y JavaScript, incluye una interfaz de usuario intuitiva y fácil de usar.",
                 "Un gestor de tareas simple, donde los usuarios pueden añadir, editar y eliminar tareas. El proyecto utiliza JavaScript para manipular las tareas y almacenar los cambios de manera eficiente.",
@@ -30,13 +30,13 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         en: {
             welcome: "Welcome to my digital world",
-            description: "Web developer in constant learning",
+            description: "Transforming ideas into digital solutions",
             projects: "My Projects",
             projectsDesc: "I have developed web projects using HTML, CSS, JavaScript, and Bootstrap to create responsive, functional, and attractive interfaces. HTML provides a clear structure, CSS enhances the visual design and adaptability, JavaScript adds interactivity, and Bootstrap ensures agile and responsive development. Each project reflects my focus on efficiency and user experience.",
             projectTitles: [
                 "Front End Store",
                 "Landing Page",
-                "Iguana Page",
+                "Musical Festival",
                 "Cuppon Page",
                 "Real Estate",
                 "Todo List",
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
             projectDescriptions: [
                 "An e-commerce project simulating an online store with a responsive design using HTML and CSS.",
                 "A simple blog developed with HTML and CSS that allows the visualization of articles related to the coffee world.",
-                "A website with an iguana theme, using HTML and CSS. The page features an attractive design with information about these creatures and an easy-to-navigate interface.",
+                "This project is an interactive website designed to promote and manage a music festival. The site offers a visual and dynamic experience for users interested in the event, with an intuitive and modern interface.",
                 "A web project simulating a coupon and discount page. It uses HTML, Bootstrap, and CSS to create a visually appealing and functional interface that allows users to view various promotions.",
                 "A real estate web application that allows showcasing properties for sale and rent. Developed with HTML, CSS, and JavaScript, it includes an intuitive and easy-to-use user interface.",
                 "A simple task manager where users can add, edit, and delete tasks. The project uses JavaScript to manipulate tasks and store changes efficiently.",
--- assets/js/script.js
+++ assets/js/script.js
@@ -37,13 +37,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const content = {
         es: {
             welcome: "Bienvenid@ a mi mundo digital",
-            description: "Desarrolladora web en constante aprendizaje",
+            description: "Transformando ideas en soluciones digitales",
             aboutTitle: "Sobre mí",
-            aboutText: `¡Hola! Soy Mariangel Araviches, una desarrolladora en constante crecimiento, dando el salto de Front-End a Fullstack con una sólida base en <strong>HTML, CSS, JavaScript y React.</strong> Actualmente, me encuentro fortaleciendo mis habilidades a través de un bootcamp de desarrollo web, siempre con la meta de expandir mi conocimiento y enfrentar nuevos desafíos.<br><br>
+            aboutText: `¡Hola! Soy Mariangel Araviches, una desarrolladora en constante crecimiento, dando el salto de Front-End a Fullstack con una sólida base en <strong>HTML, CSS, JavaScript</strong> y <strong>React.</strong> Actualmente, me encuentro fortaleciendo mis habilidades a través de un bootcamp de desarrollo web, siempre con la meta de expandir mi conocimiento y enfrentar nuevos desafíos.<br><br>
 
                 Desde 2023, he explorado el mundo de la programación mediante cursos en línea y proyectos personales, impulsada por mi pasión por la tecnología y su impacto en el mundo. Mi enfoque autodidacta y mi mentalidad de crecimiento me han permitido adaptarme a entornos dinámicos y aprender con rapidez. <br><br>
                 
-                A futuro, aspiro a especializarme en <strong>Backend y DevOps</strong>, explorando tecnologías como <strong>PHP, Node.js y PostgreSQL</strong>, junto con <strong>herramientas de automatización y despliegue</strong>. Más allá del código, disfruto sumergirme en el mundo de los videojuegos, especialmente los clásicos retro, y en series animadas como Daria. <br><br>
+                A futuro, aspiro a especializarme en <strong>Backend y DevOps</strong>, explorando tecnologías como <strong>PHP, Node.js</strong> y <strong>PostgreSQL</strong>, junto con <strong>herramientas de automatización y despliegue</strong>. Más allá del código, disfruto sumergirme en el mundo de los videojuegos, especialmente los clásicos retro, y en series animadas como Daria. <br><br>
                 
                 ¿Trabajamos junt@s? Escríbeme a <strong> <a href="mailto:mariangelaraviches@gmail.com">mariangelaraviches@gmail.com</a> 🚀
                 </strong>`,
@@ -54,22 +54,22 @@ document.addEventListener("DOMContentLoaded", () => {
             project2Title: "Landing Page",
             project2Description: "Un blog sencillo desarrollado con HTML y CSS que permite la visualización de artículos relacionados con el mundo del café.",
             project2Link: "https://mangellch.github.io/blogdecafe/",
-            project3Title: "Iguana Page",
-            project3Description: "Un sitio web con temática de iguanas, utilizando HTML y CSS. La página presenta un diseño atractivo con información sobre estas criaturas y una interfaz fácil de navegar.",
-            project3Link: "https://mangellch.github.io/iguanapage/",
+            project3Title: "Festival De Musica",
+            project3Description: "This project is an interactive website designed to promote and manage a music festival. The site offers a visual and dynamic experience for users interested in the event, with an intuitive and modern interface.",
+            project3Link: "https://github.com/Mangellch/rock-metal-festival.git",
             project4Title: "Cuppon Page",
             project4Description: "Un proyecto web que simula una página de cupones y descuentos. Utiliza HTML, Bootstrap y CSS para crear una interfaz visual atractiva y funcional que permite a los usuarios visualizar diversas promociones.",
             project4Link: "https://mangellch.github.io/paginacuppon/",
         },
         en: {
             welcome: "Welcome to my digital world",
-            description: "eb developer in constant learning",
+            description: "Transforming ideas into digital solutions",
             aboutTitle: "About me",
-            aboutText: `Hello! I am Mariangel Araviches, a developer in constant growth, making the leap from Front-End to Fullstack with a solid foundation in <strong>HTML, CSS, JavaScript and React.</strong> Currently, I am strengthening my skills through a web development bootcamp, always with the goal of expanding my knowledge and facing new challenges.<br><br>
+            aboutText: `Hello! I am Mariangel Araviches, a developer in constant growth, making the leap from Front-End to Fullstack with a solid foundation in <strong>HTML, CSS, JavaScript</strong> and <strong>React.</strong> Currently, I am strengthening my skills through a web development bootcamp, always with the goal of expanding my knowledge and facing new challenges.<br><br>
 
             Since 2023, I have explored the world of programming through online courses and personal projects, driven by my passion for technology and its impact on the world. My self-directed approach and growth mindset have allowed me to adapt to dynamic environments and learn quickly. <br><br>
             
-            In the future, I aspire to specialize in <strong>Backend and DevOps</strong>, exploring technologies such as <strong>PHP, Node.js and PostgreSQL</strong>, along with <strong>automation and deployment tools</strong> . Beyond code, I enjoy immersing myself in the world of video games, especially retro classics, and animated series like Daria. <br><br>
+            In the future, I aspire to specialize in <strong>Backend</strong> and <strong>DevOps</strong>, exploring technologies such as <strong>PHP, Node.js</strong> and <strong>PostgreSQL</strong>, along with <strong>automation</strong> and <strong>deployment tools</strong> . Beyond code, I enjoy immersing myself in the world of video games, especially retro classics, and animated series like Daria. <br><br>
             
              Do we work together? Write to me at <strong> <a href="mailto:mariangelaraviches@gmail.com">mariangelaraviches@gmail.com</a> 🚀
                 </strong>`,
@@ -80,9 +80,9 @@ document.addEventListener("DOMContentLoaded", () => {
             project2Title: "Landing Page",
             project2Description: "A simple blog developed with HTML and CSS that allows the visualization of articles related to the coffee world.",
             project2Link: "https://mangellch.github.io/blogdecafe/",
-            project3Title: "Iguana Page",
-            project3Description: "A website with an iguana theme, using HTML and CSS. The page features an attractive design with information about these creatures and an easy-to-navigate interface.",
-            project3Link: "https://mangellch.github.io/iguanapage/",
+            project3Title: "Musical Festival",
+            project3Description: "This project is an interactive website designed to promote and manage a music festival. The site offers a visual and dynamic experience for users interested in the event, with an intuitive and modern interface.",
+            project3Link: "https://github.com/Mangellch/rock-metal-festival.git",
             project4Title: "Cuppon Page",
             project4Description: "A web project simulating a coupon and discount page. It uses HTML, Bootstrap, and CSS to create a visually appealing and functional interface that allows users to view various promotions.",
             project4Link: "https://mangellch.github.io/paginacuppon/",
